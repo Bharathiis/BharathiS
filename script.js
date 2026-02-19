7ad0f4f3a0cf5c43a325a9d5a40b557c7a466a01
Bharathiis <2023me0373@svce.ac.in>
2026-02-20 00:32:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-// Hearts On Click
+// Welcome Click
+document.getElementById("welcome").addEventListener("click", function(){
+this.style.display="none";
+document.getElementById("main").classList.remove("hidden");
+});
+
+// Show Section
+function showSection(id){
+let sections=document.querySelectorAll(".content");
+sections.forEach(sec=>sec.style.display="none");
+document.getElementById(id).style.display="block";
+}
+
+// Hearts Animation
 document.addEventListener("click", function(e){
-
-let heart = document.createElement("div");
-heart.innerHTML = "❤️";
+let heart=document.createElement("div");
+heart.innerHTML="💕";
 heart.classList.add("heart");
-
-heart.style.left = e.clientX + "px";
-heart.style.top = e.clientY + "px";
-
+heart.style.left=e.clientX+"px";
+heart.style.top=e.clientY+"px";
 document.body.appendChild(heart);
-
-setTimeout(()=>{
-heart.remove();
-},2000);
-
+setTimeout(()=>heart.remove(),2000);
 });
